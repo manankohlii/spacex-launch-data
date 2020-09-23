b5f92ed3368eea6ed722853450b395717712993c
Manan Kohli <mankoh00@gmail.com>
2020-09-23 17:18:39 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let fnum;
+function fn(){
+    var fnumber=document.getElementById("fnumber").value;
+    fnum=fnumber;
+    getData();
+}
+let url= "https://api.spacexdata.com/v3/launches/"
+function getData(){
+fetch(url+fnum).then(function(response){
+    return response.json();
+}).then(function(obj){
+    console.log(obj);
+    document.getElementById("number").textContent=obj.flight_number;
+    
+    document.getElementById("year").textContent=obj.launch_year;
+    document.getElementById("mname").textContent=obj.mission_name;
+    document.getElementById("rname").textContent=obj.rocket.rocket_name;
+    document.getElementById("lsite").textContent=obj.launch_site.site_name_long;
+
+}).catch(function(error){
+    console.error('wrong');
+    console.error(error);
+});
+}
