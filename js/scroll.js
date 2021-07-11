6f9770d658ce4d9f1f481e98933156048562b12e
pranjay-poddar <pranjaypoddar@outlook.com>
2021-07-11 14:57:00 +0530

    added sound effects and animations

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,10 @@
 const toTop = document.querySelector(".to-top");
 
-window.addEventListener("scroll", () =>
-{
-    if(window.pageYOffset > 100) {
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
         toTop.classList.add("active");
 
-    }
-    else{
+    } else {
         toTop.classList.remove("active");
     }
 
